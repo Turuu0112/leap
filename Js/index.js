7bf03444c95a593f4b7c30fd7fc8cf35986d7ac0
24LP1247 24LP1247 <24LP1247@G301-CC0210.local>
2024-05-21 12:42:39 +0800

    tuesday

--- Js/index.js
+++ Js/index.js
@@ -1,11 +1,16 @@
 //openning line
-let a ="Comments can make code readable"
-var b = "Comments can make code"
-const c = " Javascript lesson begin"
-
-
-console.log(b + a + c);
-console.log("Comments can make code readable");
-console.log(b)
-console.log(a)
-console.log(c)
+let a ="Hello world"
+let b ="aZZZbZZZc"
+let d ="I love HTML"
+let c = "deviceName : macbook air -- deviceSerial : FFM3JHQ6L7 "
+let ch="Challenge"
+const PI=Math.PI;
+const rndNum=Math.random()
+console.log("random:","rndNum")
+console.log("geometrical constant:",PI)
+console.log("roundup",Math.round(9.76))
+console.log("rounddown",Math.round(PI))
+console.log(c.split("--")[1].replace(":","="))
+console.log(d.replace("HTML","JAVASCRIPT").repeat(10))
+console.log(b.split("ZZZ"))
+console.log(a.substring(5,9).toUpperCase())
