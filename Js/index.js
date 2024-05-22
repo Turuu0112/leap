95849f8270495e0133802a159dcc2f4bebf76013
24LP1247 24LP1247 <24LP1247@G301-CC0210.local>
2024-05-22 12:51:55 +0800

    hi

--- Js/index.js
+++ Js/index.js
@@ -3,13 +3,28 @@ let a ="Hello world"
 let b ="aZZZbZZZc"
 let d ="I love HTML"
 let c = "deviceName : macbook air -- deviceSerial : FFM3JHQ6L7 "
-let ch="Challenge"
-const PI=Math.PI;
-const rndNum=Math.random()
-console.log("random:","rndNum")
-console.log("geometrical constant:",PI)
-console.log("roundup",Math.round(9.76))
-console.log("rounddown",Math.round(PI))
+let cH ="Challenge can make something"
+let social ="facebook,google,microsoft,apple,IBM,oracle,Amazon"
+let tR = "  I love HTML.I love Javascript.Hello new world.Time to sleep.   "
+let arrya1 = "I love"
+let arrya2 ="Javascript"
+let array3 = arrya1.concat(arrya2)
+let pineCone ="Pinecone Academy Leap хөтөлбөр"
+
+console.log(cH.toUpperCase())
+console.log(cH.toLowerCase())
+console.log(cH.substring(9))
+console.log(cH.substring(16))
+console.log(cH.includes('script'))
+console.log(cH.split(" "))
+console.log(cH.split())
+console.log(social.split(","))
+console.log(cH.replace("Challenge can make something","ahahha"))
+console.log(cH.charAt(15))
+console.log(tR.trim())
+console.log(array3)
+console.log(pineCone.repeat(18))
+
 console.log(c.split("--")[1].replace(":","="))
 console.log(d.replace("HTML","JAVASCRIPT").repeat(10))
 console.log(b.split("ZZZ"))
---
+++ Js/jsnumber.js
@@ -0,0 +1,35 @@
+// const PI=Math.PI;
+
+// const num = 10;
+// const nums = 10;
+// let word = "Phython jargon"
+// let words ="I hope this course is not full of jargon"
+// console.log(num ==nums)
+// console.log(word.includes("on"))
+// console.log(word.includes("jargon"))
+
+
+
+
+// const randNum=Math.random()*(0-100);
+// const randNum2=Math.random()*(50-100)
+// const randNum3=Math.random()*(255)
+// const jS = Math.floor(Math.random() * (9));
+// const script = "Javascript"
+// const result = jS
+// console.log("random:",randNum)
+// console.log("random:",randNum2)
+// console.log("random:",randNum3)
+// console.log("javasc",jS)
+// console.log("geometrical constant:",PI)
+// console.log("roundup",Math.round(9.76))
+// console.log("rounddown",Math.round(PI))
+const now = new Date()
+const year = now.getFullYear()+1;
+const month = now.getMonth()
+const date = now.getDate()
+const hours = now.getHours()
+const  minutes= now.getMinutes()
+const  seconds =now.getSeconds()
+
+console.log(`Өнөөдөр ${year} оны${month}- рсарын ${date} өдөр  Одоо${hours} цаг${minutes} мин${seconds} секунд болж байна.)
