1887ee5631a86217451a2791efccf057493e8538
24LP1247 24LP1247 <24LP1247@G301-CC0210.local>
2024-05-27 12:51:27 +0800

    hi

--- Js/jsnumber.js
+++ Js/jsnumber.js
@@ -8,9 +8,6 @@
 // console.log(word.includes("on"))
 // console.log(word.includes("jargon"))
 
-
-
-
 // const randNum=Math.random()*(0-100);
 // const randNum2=Math.random()*(50-100)
 // const randNum3=Math.random()*(255)
@@ -24,12 +21,145 @@
 // console.log("geometrical constant:",PI)
 // console.log("roundup",Math.round(9.76))
 // console.log("rounddown",Math.round(PI))
-const now = new Date()
-const year = now.getFullYear()+1;
-const month = now.getMonth()
-const date = now.getDate()
-const hours = now.getHours()
-const  minutes= now.getMinutes()
-const  seconds =now.getSeconds()
+// const now = new Date();
+// const year = now.getFullYear() + 1;
+// const month = now.getMonth();
+// const date = now.getDate();
+// const hours = now.getHours();
+// const minutes = now.getMinutes();
+// const seconds = now.getSeconds();
 
-console.log(`Өнөөдөр ${year} оны${month}- рсарын ${date} өдөр  Одоо${hours} цаг${minutes} мин${seconds} секунд болж байна.)
+// console.log(`Өнөөдөр ${year} оны${month}- рсарын ${date} өдөр  Одоо${hours} цаг${minutes}
+// мин${seconds} секунд болж байна.`);
+
+// let x = 11;
+// let y = 4;
+
+// if (x <= 10 && y >= 4) {
+//   console.log("hello");
+// }
+
+// if (x <= 10 || y >= 4) {
+//   console.log("hello baby");
+// }
+
+// if (x !== 11) {
+//   console.log("11 bish bn sh t");
+// } else {
+//   console.log("hi 11 ee");
+//   let nas =6;
+//   let gender ="male"
+//   if (nas>= 6 &&  gender === "male" ){
+//     console.log("8-r angiin eregtei suragch");
+//   }
+//   if(nas>=6 || gender ==="female"){
+//     console.log("8-r angiin emegtei suragch")
+//   }
+//   if (nas !== 6){
+//     console.log("suragch bish")
+//   }else {
+//     console.log("suragch ")
+//   }
+// let i = 4 > 3; // true
+// let ii = 4 < 3; // false
+// let iii = 4 >= 3;// true
+// let iv = 4 <= 3;// false
+// let v = 4 == 4;// true
+// let vi = 4 === 4;// true
+// let vii = 4 != 4;// true
+// let iix = 4 !== 4;// false
+// let ix = 4 != "4";// true
+// let x = 4 == "4"; //false 
+// let xi = 4 === "4";//false
+// let one = 4 > 3 && 10 < 12;//true 
+// let two = 4 > 3 && 10 > 12;// false 
+// let three = 4 > 3 || 10 < 12;// true 
+// let four = 4 > 3 || 10 > 12;// true
+// let five = !(4 > 3);// false
+// let six = !(4 < 3); //true 
+// let eight = !(4 > 3 && 10 < 12);//false
+// let nine = !(4 > 3 && 10 > 12);//true
+// let ten = !(4 === "4");//false
+//          
+
+
+                // Array 
+// const number=[1,3,45,5 ,56 ,65,767]
+
+// // let num = number.pop()
+// let mum = number.push(25,78)
+// // const g =new Array() == []
+
+// console.log("Number",mum)
+// console.log("Numbers",number);
+
+
+
+// function count(n){
+//     for (let i=1;i<=n;i+=2){
+//         console.log(i)
+//         console.log("hello")
+//     }
+// }
+// count(8)
+
+// function count(n){
+//     const arr = []
+//     for (let i=1;i<=n;i++){
+//         arr.push(i)
+//         // console.log("loop",arr)
+
+//     }
+//     return arr;
+
+// }
+// // console.log(arr)
+// const arr = count(7)
+// const a = count(11)
+// console.log(arr,a); 
+
+
+// const reverse =(n)=>{
+//     const arr =[]
+//     for(let i = n;i>0; i--){
+//         arr.push(i)
+
+//     }
+//         return arr;
+// }
+
+// console.log(reverse(15))
+
+// const a= [1,5,2,7,45,13,13];
+
+// function printArr(arr){
+//    for( let i = 0; i< arr.length; i++){
+//     console.log(arr(i))
+//    }
+// }
+
+// printArr(a)
+
+// const a = [1,5,2,7,45,5,12];
+// function countEven(arr){
+//     let count = 0
+//     for( let i = 0; i<arr.length; i++){
+//         if(arr[i] % 2 ==0){
+//             count++;
+//         }
+//         return count
+
+//     }
+//     }
+// const numberOfEvenes = countEven(a)
+// console.log(numberOfEvenes);
+
+const a = [1,334, 23, 2322, 2,1];
+function max(arr){
+    
+
+        }
+}
+
+const result = max(a)
+console.log(result)
