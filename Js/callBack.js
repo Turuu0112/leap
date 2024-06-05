176229a573b0a5a9c8b4d9e9ec93574b10455d03
24LP1247 24LP1247 <24LP1247@G301-CC0210.local>
2024-06-05 12:28:20 +0800

    hhh

--- Js/callBack.js
+++ Js/callBack.js
@@ -51,7 +51,7 @@
 //  });
 //  console.log(element)
 // let grades = [45, 54, 54, 54, 22]
- 
+
 
 // const roundGrades  =grades.map((grade) =>{
 //     if(grade < 38 ) {
@@ -62,27 +62,97 @@
 // })
 // console.log(roundGrades(grades))
 
-const arr = [ 1, 1, 3, 4, 4, ]
+// const arr = [ 1, 1, 3, 4, 4, ]
 //  const filterArr = arr.filter((el) => {
 //     return el % 2 === 1
 //  })
 //  console.log(filterArr)
 
-const sum  = arr.reduce((acc, current ) => {
-    return acc + current
-
-},0)
-console.log(sum)
 
 
-const max  = arr.reduce((acc, current ) => {
-    if (acc < current) return current
-     
-    return acc
+// const sum  = arr.reduce((acc, current ) => {      // niilberiig oloh
+//     return acc + current
 
-},arr[0])
-console.log(max)
+// },0)
+// console.log(sum)
 
+
+
+
+// const max  = arr.reduce((acc, current ) => {  // Maxiig oloh
+//     if (acc < current) return current
+
+//     return acc
+
+// },arr[0])
+// console.log(max)
+
+
+let numbers = [5,2,7,5,9,] 
+
+const swap =  (arr, i, j) =>{    
+let res = arr [i]
+arr[i] = arr[j]                             // Bubble sort
+arr[j] = res
+
+}
+// const bubbleSort = (arr) =>{
+//     for(let k=0; k< arr.length; k++){
+//         for (let i = 0; i<arr.length-1-i; i++){
+//             if(arr[i] > arr[i+1] ){
+//                 swap (arr, i ,i+1)
+//             }
+//         }
+//     }
+// }
+// bubbleSort(numbers)
+// console.log(numbers)
+
+
+const finIndexMIn = (arr,start) =>{
+    let minIndex = start;
+
+    for (let i = start+1 ; i < arr.length ; i++){
+        if (arr[i] < arr.minIndex){
+            minIndex = i
+        }
+    }
+    return minIndex
+}
+console.log(finIndexMIn(numbers))
+
+const selectionSort = (arr) =>{
+    for( i = 0; i < arr.length ; i ++){
+        let minIndex = finIndexMIn (arr,i)
+        swap (arr, i ,minIndex)
+
+    }
+
+    }
+    console.log(numbers)
+
+    selectionSort(numbers)
+    console.log(numbers)
+
+
+
+    const arr = [ "a", "abg", "dwdwdw", "f"]
+    arr.sort ((a,b) => a.length -b.length)
+
+    console.log("sort", arr)
+
+    const a = [ "a", "abg", "dwdwdw", "f"]
+    arr.sort ((a,b) => {
+        if (a > b) return 1
+        if ( a < b) return -1
+        return 0
+
+    })
+
+    console.log("sort", a)
+
+    // b = a > 5? true : false ; // ternory 
+    // b = a > 5? true : a > 3 ? false : null
 
 
 
