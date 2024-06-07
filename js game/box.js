471bbd11599c46e42d8da114d1c44596cc22e487
24LP1247 24LP1247 <24LP1247@G301-CC0210.local>
2024-06-07 12:34:55 +0800

    hahah

---
+++ js game/box.js
@@ -0,0 +1,48 @@
+// const box = document.getElementsByClassName("box")[0]
+
+// box.addEventListener("click", () => {
+//     if(box.style.backgroundColor === "red"){
+//         box.style.backgroundColor ="#181818";
+
+//     }else
+//     box.style.backgroundColor = "red"
+// })
+
+// const box = document.querySelector(".box");
+// const [minusButton, plusButton] = document.querySelectorAll(".buttons button");
+// let num = 0;
+// box.textContent = num;
+
+// minusButton.addEventListener("click", () => {
+//   num--;
+//   box.textContent = num;
+// });
+// plusButton.addEventListener("click", () => {
+//   num++;
+//   box.textContent = num;
+// });
+
+// const boxes = document.querySelectorAll(".box-container > div")
+// const restartbtn = document.querySelector(".button")
+// let players = "X"
+// boxes.forEach((box) =>{
+//     box.addEventListener("click" , () =>{
+//        if(box.textContent === ''){
+//         box.content = players; 
+//        }
+//     })
+// })
+
+const images = document.querySelectorAll(".image > div") // CAROUSELS
+const [Prevbutton, Nextbutton] = document.querySelectorAll(".buttons button")
+
+
+let index = 0;
+
+Prevbutton.addEventListener("click" , ()=>{
+
+})
+
+
+
+
