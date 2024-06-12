d9fc96666f11ace65056a4006c098f13e3851783
24LP1247 24LP1247 <24LP1247@G301-CC0210.local>
2024-06-12 13:03:41 +0800

    hha

--- sign.up/hello.js
+++ sign.up/hello.js
@@ -1,14 +1,32 @@
-const signUp = document.querySelector(".Sign-up button")
-const right = document.querySelector(".right")
-const box = document.querySelector(".box")
-const sigign =document.querySelector(".Sign-ign button")
-const left = document.querySelector(".left")
+const signUp = document.querySelector(".Sign-up button");
+const right = document.querySelector(".right");
+const box = document.querySelector(".box");
+const sigign = document.querySelector(".Sign-ign button");
+const left = document.querySelector(".left");
 
-signUp.addEventListener("click",()=>{
-    right.classList.add("change")
-    
-})
-sigign.addEventListener("click",()=>{
-    left.classList.add("left-change")
-})
 
+signUp.addEventListener("mousedown", () => {
+  right.classList.add("change",);
+  left.classList.add("left-change",)
+
+
+});
+sigign.addEventListener("click", () => {
+  left.classList.remove("left-change");
+  right.classList.remove("change")
+
+
+
+});
+
+// const container = document.getElementById("container");
+// const signUpButton = document.getElementById("signUp");
+// const signInButton = document.getElementById("signIn");
+
+// signUpButton.addEventListener("click", () => {
+//   container.classList.add("change");
+// });
+
+// signInButton.addEventListener("click", () => {
+//   container.classList.remove("change");
+// });
